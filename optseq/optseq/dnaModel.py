79ddac9dc553e936915baee77d11f6ecf572c7d2
Tileli Amimeur <Leli@D-108-179-180-197.dhcp4.washington.edu>
2017-04-25 17:15:05 -0700

    added start of CNN hyperparam tuning

--- optseq/optseq/dnaModel.py
+++ optseq/optseq/dnaModel.py
@@ -1,14 +1,58 @@
 import numpy as np 
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution1D
+from keras.optimizers import RMSprop, SGD, Adam
+from keras.wrappers.scikit_learn import KerasRegressor
 import random
 import pandas as pd
+from sklearn.model_selection import GridSearchCV
 from sklearn.cross_validation import train_test_split
 from sklearn import preprocessing
+from sklearn.metrics import make_scorer
 import scipy.stats as stats
 import logging
 
 np.random.seed(1337)
+
+#this Needs to change
+#seq_len = 150 
+
+def create_model(learn_rate=0.001, seq_len=15):
+	########## Build CNN Model #############
+	cnn = Sequential()
+	cnn.add(Convolution1D(nb_filter=30,filter_length=6,input_dim=4,input_length=seq_len,border_mode="same", activation='relu'))
+	cnn.add(Dropout(0.1))
+	cnn.add(Convolution1D(nb_filter=40,filter_length=6,input_dim=4,input_length=seq_len,border_mode="same", activation='relu'))
+
+	cnn.add(Flatten())
+
+	#this dense layer should be tuned to various percentages
+	#of the first input layer, like [(seq_len*2)/3, seq_len/3]
+	cnn.add(Dense(40))
+	cnn.add(Dropout(0.2))
+	cnn.add(Activation('relu'))
+
+	cnn.add(Dense(1))
+	cnn.add(Activation('linear'))
+
+	#compile the model
+	adam = Adam(lr=learn_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+	#model.compile(loss='mean_squared_error', optimizer=adam)
+	rms = RMSprop(lr=learn_rate, rho=0.9, epsilon=1e-08)
+
+	#optimizer, lr, and momeuntum should be tuned
+	#need a custom metric (r^2 methinks)
+	cnn.compile(loss='mean_squared_logarithmic_error', optimizer=rms)#, metrics=['accuracy'])
+	#print 'Model compiled in {0} seconds'.format(time.time() - start_time)
+	return cnn
+
+def my_custom_r2_func(ground_truth, predictions):
+	slope, intercept, r_value, p_value, std_err = stats.linregress(ground_truth.reshape(-1),predictions.reshape(-1))
+	print "Evaluating a model meow: ", r_value**2
+	return r_value**2
+	# diff = np.abs(ground_truth - predictions).max()
+	# return np.log(1 + diff)
 
 class dnaModel(object):
 	""" A CNN model for DNA sequences """
@@ -18,13 +62,14 @@ class dnaModel(object):
 		#logging.basicConfig(filename='optseq.log',level=logging.DEBUG)
 		#logging.info("test")
 		self.model, self.X_train, self.Y_train, self.X_test, self.Y_test, self.batch, self.epoch = \
-				self.__create_model(input_file)
+				self.__opt_model(input_file)
 		
 
 	###inits/bulids a new model and optimizes hyperparams
 	###trains/fits and saves hyperparams of training to dnaModel members
-
-	def __create_model(self, input_file):
+	###currently a simple gridsearch, but should switch to a stochaistic 
+	###optimization alg with mean performance as the cost fcn
+	def __opt_model(self, input_file):
 		xtrain, ytrain, xtest, ytest = [], [], [], []
 
 		raw_data = pd.read_excel(input_file, header=0, parse_cols="A,B")
@@ -35,7 +80,7 @@ class dnaModel(object):
 
 		############# Format NN inputs #############
 		seq_len = len(df['sequence'][0])
-		X_data = np.empty([len(df),150,4])
+		X_data = np.empty([len(df),seq_len,4])
 		indx = 0
 
 		Y_data = np.array(df[[u' expression']])
@@ -48,35 +93,33 @@ class dnaModel(object):
 		# print "CNN output \n", Y_data
 
 		########## RANDOM TEST/TRAIN SPLIT #########
-		xtrain, xtest, ytrain, ytest = train_test_split(X_data, Y_data, test_size=0.15, random_state=42)
-		norm_train = preprocessing.StandardScaler().fit_transform(ytrain)
+		normed_out = preprocessing.StandardScaler().fit_transform(Y_data)
+		xtrain, xtest, ytrain, ytest = train_test_split(X_data, normed_out, test_size=0.15, random_state=42)
+		# xtrain, xtest, ytrain, ytest = train_test_split(X_data, Y_data, test_size=0.15, random_state=42)
+		# norm_train = preprocessing.StandardScaler().fit_transform(ytrain)
 
-		########## Build CNN Model #############
-		cnn = Sequential()
-		cnn.add(Convolution1D(nb_filter=30,filter_length=6,input_dim=4,input_length=150,border_mode="same", activation='relu'))
-		cnn.add(Dropout(0.1))
-		cnn.add(Convolution1D(nb_filter=40,filter_length=6,input_dim=4,input_length=150,border_mode="same", activation='relu'))
-
-		cnn.add(Flatten())
-
-		cnn.add(Dense(40))
-		cnn.add(Dropout(0.2))
-		cnn.add(Activation('relu'))
-
-		cnn.add(Dense(1))
-		cnn.add(Activation('linear'))
-
-		#compile the model
-		adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-		#model.compile(loss='mean_squared_error', optimizer=adam)
-		rms = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08)
-		cnn.compile(loss='mean_squared_logarithmic_error', optimizer=rms)
-		#print 'Model compiled in {0} seconds'.format(time.time() - start_time)
+		model = KerasRegressor(build_fn=create_model, nb_epoch=6, batch_size=128, verbose=1)
+		# define the grid search parameters
+		num_bases = [seq_len]
+		learn_rate = [0.001, 0.01, 0.1]
+		#momentum = [0.0, 0.2, 0.4, 0.6, 0.8, 0.9]
+		param_grid = dict(learn_rate=learn_rate, seq_len = num_bases)#, momentum=momentum)
+		#specify my own scorer for GridSearchCV that uses r2 instead of the estimator's scorer
+		grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=make_scorer(my_custom_r2_func, greater_is_better=True), n_jobs=1)
+		grid_result = grid.fit(xtrain, ytrain)
+		# summarize results
+		print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+		means = grid_result.cv_results_['mean_test_score']
+		stds = grid_result.cv_results_['std_test_score']
+		params = grid_result.cv_results_['params']
+		for mean, stdev, param in zip(means, stds, params):
+		    print("%f (%f) with: %r" % (mean, stdev, param))
 
 		########### Train CNN model ############
-		cnn.fit(X_train, norm_train, batch_size=128, nb_epoch=6, verbose=1)
+		#batch_size and epoch should be tuned
+		#cnn.fit(xtrain, norm_train, batch_size=128, nb_epoch=6, verbose=1)
 
-		return cnn, xtrain, ytrain, xtest, ytest, 128, 6
+		return model, xtrain, ytrain, xtest, ytest, 128, 6
 
 	def __oneHotEncoder(self,seq):
 		base_dict = {u'A':[1,0,0,0],u'C':[0,1,0,0],u'G':[0,0,1,0],u'T':[0,0,0,1]}
